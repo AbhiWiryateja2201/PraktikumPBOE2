6e2baa0521b9c3f501ecc4e095619d67ffdcbfad
Paradox <123134773+AbhiWiryateja2201@users.noreply.github.com>
2026-03-12 11:08:00 +0700

    ini dulu

---
+++ Pertemuan4/src/Persegi.java
@@ -0,0 +1,47 @@
+public class Persegi extends BangunDatar
+{
+    private double sisi;
+
+    public Persegi(){
+        setJmlSisi(4); // Persegi memiliki 4 sisi
+    }
+
+    public Persegi(double sisi, String warna, String border){
+        this.sisi = sisi;
+        setWarna(warna);
+        setBorder(border);
+    }
+
+    public double getSisi(){
+        return sisi;
+    }
+
+    public void setSisi(double sisi)
+    {
+        this.sisi = sisi;
+    }
+
+    public double getLuas()
+    {
+        return sisi * sisi;
+    }
+
+    public double getKeliling()
+    {
+        return 4 * sisi;
+    }
+
+    public double getDiagonal()
+    {
+        return sisi * Math.sqrt(2);
+    }
+
+    @Override
+    public void printInfo()
+    {
+        super.printInfo();
+        System.out.println("Sisi: " + sisi);
+        System.out.println("Luas: " + getLuas());
+        System.out.println("Keliling: " + getKeliling());
+    }   
+}
